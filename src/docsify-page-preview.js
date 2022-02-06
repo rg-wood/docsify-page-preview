a15e52c57e7d624f5d8b1ce1d53de22db033281a
R.G. Wood <ric@grislyeye.com>
2022-02-06 12:36:10 +0000

    Fix: popups cut off by right view screen border

--- src/docsify-page-preview.js
+++ src/docsify-page-preview.js
@@ -4,13 +4,67 @@ import { cached } from './cached.js'
 import './dom-structure.js'
 
 class DocsifyPagePreview extends HTMLElement {
+
+  static defaultMinWidth = 300
+  static defaultMinHeight = 200
+  static defaultMaxWidth = 450
+  static defaultMinHeight = 200
+
+  static popupStyle = `
+    position: absolute;
+
+    min-width: var(--docsify-page-preview-min-width, ${this.defaultMinWidth}px);
+    min-height: var(--docsify-page-preview-min-height, ${this.defaultMinHeight}px);
+    max-width: var(--docsify-page-preview-max-width, ${this.defaultMaxWidth}px);
+    max-height: var(--docsify-page-preview-max-height, ${this.defaultMinHeight}px);
+
+    background: var(--docsify-page-preview-background, white);
+    border-radius: .4em;
+    border: solid var(--theme-color, #42b983);
+    box-shadow:
+      0 2.8px 2.2px rgba(0, 0, 0, 0.034),
+      0 6.7px 5.3px rgba(0, 0, 0, 0.048),
+      0 12.5px 10px rgba(0, 0, 0, 0.06),
+      0 22.3px 17.9px rgba(0, 0, 0, 0.072),
+      0 41.8px 33.4px rgba(0, 0, 0, 0.086),
+      0 100px 80px rgba(0, 0, 0, 0.12);
+
+    margin-top: 1em;
+    padding: 1em;
+    z-index: 1;
+
+    pointer-events: none;
+    overflow: hidden;
+  `
+
   constructor () {
     super()
     this.page = this.getAttribute('page')
     this.ref = this.getAttribute('ref')
+    this.parser = cached(marked.parse)
+
     this.addEventListener('mouseover', this.showPreview)
     this.addEventListener('mouseout', this.hidePreview)
-    this.parser = cached(marked.parse)
+  }
+
+  get popupVerticalOffset() {
+    const computedMaxWidth = window
+      .getComputedStyle(document.body)
+      .getPropertyValue('--docsify-page-preview-max-width')
+
+    const maxWidth = computedMaxWidth
+      ? computedMaxWidth
+      : DocsifyPagePreview.defaultMaxWidth
+
+    const containerBox = document.getElementById('main').getBoundingClientRect()
+    const containerWidth = containerBox.width
+    const tooFarLeft = this.offsetLeft + maxWidth > containerWidth
+
+    if(tooFarLeft) {
+      return this.offsetLeft - maxWidth
+    } else {
+      return this.offsetLeft
+    }
   }
 
   async showPreview () {
@@ -34,54 +88,15 @@ class DocsifyPagePreview extends HTMLElement {
       this.append(this.popup)
     }
 
-    this.popup.style.cssText = this.popupStyle
+    this.popup.style.cssText = DocsifyPagePreview.popupStyle
+    this.popup.style.left = `${this.popupVerticalOffset}px`
     this.popup.hidden = false
-  }
-
-  get popupTop () {
-    return this.offsetTop
-  }
-
-  get popupLeft () {
-    const bodyLeft = document.body.getBoundingClientRect()
-    const elementLeft = this.getBoundingClientRect().left
-    const offset = elementLeft - bodyLeft - this.width
-
-    return offset
   }
 
   hidePreview () {
     if (this.popup) this.popup.hidden = true
   }
 
-  get popupStyle () {
-    return `
-      position: absolute;
-      max-width: var(--docsify-page-preview-max-width, 450px);
-      max-height: var(--docsify-page-preview-max-height, 200px);
-
-      background: var(--docsify-page-preview-background, white);
-      border-radius: .4em;
-      border: solid var(--theme-color, #42b983);
-      box-shadow:
-        0 2.8px 2.2px rgba(0, 0, 0, 0.034),
-        0 6.7px 5.3px rgba(0, 0, 0, 0.048),
-        0 12.5px 10px rgba(0, 0, 0, 0.06),
-        0 22.3px 17.9px rgba(0, 0, 0, 0.072),
-        0 41.8px 33.4px rgba(0, 0, 0, 0.086),
-        0 100px 80px rgba(0, 0, 0, 0.12);
-
-      margin-top: 1.5em;
-      padding: 1em;
-      z-index: 1;
-
-      pointer-events: none;
-      overflow: hidden;
-
-      top: ${this.popupTop}px;
-      left: ${this.popupLeft}px;
-    `
-  }
 }
 
 customElements.define('docsify-page-preview', DocsifyPagePreview)
